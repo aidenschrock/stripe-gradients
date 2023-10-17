607514ff6d8702856a12aac01f34083ae5de4d3f
Aiden Schrock <aidennoelschrock@gmail.com>
2023-10-17 13:32:04 -0500

    inital commit

---
+++ src/glsl/fragment.glsl
@@ -0,0 +1,13 @@
+uniform float uTime;
+uniform float progress;
+uniform vec4 uResolution;
+varying vec2 vUv;
+varying vec3 vPosition;
+varying vec3 vColor;
+float PI = 3.141592653589793238;
+
+void main()
+{
+    gl_FragColor = vec4(vUv, 0, 1);
+    gl_FragColor = vec4(vColor, 1);
+}
