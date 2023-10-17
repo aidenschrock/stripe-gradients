86262e39d4236d43a402bf4d1ee700721291f3d6
Aiden Schrock <aidennoelschrock@gmail.com>
2023-10-17 13:43:20 -0500

    update build vite config

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,9 @@ import glsl from "vite-plugin-glsl";
 export default {
   root: "src/",
   plugins: [glsl()],
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    sourcemap: true,
+  },
 };
